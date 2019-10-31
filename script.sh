29c1f47f4336c0b0a1c3029f9c9d49d430365c7a
Vitaly Sinev <vitalysinev@MacBook-Pro-Vitaly.local>
2019-10-31 15:44:45 +0300

    edit script

--- script.sh
+++ script.sh
@@ -3,9 +3,9 @@
 
 while true
 do
-if [[ $(ls /app/src/ |wc -l) == 0 ]]; then
+if [ -f /app/src/*.jpg ]; then
   sleep 0.01
 else
-  mv $(ls -atr /app/src/*.jpg |tail -1) /var/www/image.jpg && rm -rf /app/src/*
+  mv $(ls -atr /app/src/*.jpg |tail -1) /var/www/image.jpg && rm -rf /app/src/*.jpg
 fi
 done
