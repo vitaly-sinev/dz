15e479695d05d83e94120d69b61a12657caa4678
Vitaly Sinev <vitalysinev@MacBook-Pro-Vitaly.local>
2019-11-01 16:39:23 +0300

    mk2

--- script.sh
+++ script.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/bash
 
+SRC_DIR="/app/src"
+WWW_DIR="/var/www"
+#PREFIX_JPG="bunny_"
 
+echo container start
 while true
 do
-if [ -f /app/src/*.jpg ]; then
+if [[ $(ls ${SRC_DIR}/bunny_*.jpg 2> /dev/null|wc -l) == 0 ]]; then
   sleep 0.01
 else
-  mv $(ls -atr /app/src/*.jpg |tail -1) /var/www/image.jpg && rm -rf /app/src/*.jpg
+  mv $(ls -atr ${SRC_DIR}/bunny_*.jpg |tail -1) ${WWW_DIR}/image.jpg && rm -rf ${SRC_DIR}/${PREFIX_JPG}*.jpg
 fi
 done
